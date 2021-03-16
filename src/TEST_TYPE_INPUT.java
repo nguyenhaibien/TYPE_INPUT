fcb121cd4f025e235369e40f3fc3c51f55d231b9
nguyenhaibien <nhbien123@gmail.com>
2021-03-16 13:49:39 +0700

    Initial commit

---
+++ src/TEST_TYPE_INPUT.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TEST_TYPE_INPUT {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        float b;
+        a = scanner.nextInt();
+        b = scanner.nextFloat();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+    }
+}
